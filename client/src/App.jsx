d566a459d21f68d0e62ba1fcd65e0279d005690b
viswanath <viswa9167@gmail.com>
2024-10-16 19:26:28 +0530

    configured frontend and backend with shadcnUI

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Login</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  mongoose
+    .connect(
+      "mongodb+srv://viswa9167:baY8AMDowswnM0j2@ecommerce.sl4ie.mongodb.net/"
+    )
+    .then(() => {
+      console.log("Database Connnected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log("server is listening on port 4000");
+});
+
+app.use(
+  cors({
+    origin: "http://http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-type",
+      "Authorization",
+      "Cache-control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
